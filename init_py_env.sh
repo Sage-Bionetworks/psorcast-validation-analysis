b4b461678b65e032b4f6afa176a4e57a4096198a
arytontediarjo <atediarjo@gmail.com>
2021-06-24 11:36:32 -0700

    update requirements.txt

--- init_py_env.sh
+++ init_py_env.sh
@@ -1,5 +1,5 @@
 python3 -m venv ./env\
     && source env/bin/activate \
     && python3 -m pip install --upgrade pip\
+    && python3 -m pip install git+https://github.com/arytontediarjo/PDKitRotationFeatures.git\
     && python3 -m pip install -r requirements.txt
-    && python3 -m pip install git+https://github.com/arytontediarjo/PDKitRotationFeatures.git
