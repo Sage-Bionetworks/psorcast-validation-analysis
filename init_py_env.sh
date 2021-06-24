744c3d2a632f414bdf44120c7b6687b1c767a9e5
arytontediarjo <atediarjo@gmail.com>
2021-06-24 11:37:41 -0700

    change path

--- init_py_env.sh
+++ init_py_env.sh
@@ -1,4 +1,4 @@
-python3 -m venv ./env\
+python3 -m venv env\
     && source env/bin/activate \
     && python3 -m pip install --upgrade pip\
     && python3 -m pip install git+https://github.com/arytontediarjo/PDKitRotationFeatures.git\
