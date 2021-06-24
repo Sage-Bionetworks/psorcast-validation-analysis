2cfd9e6a5cee9fb22d1b3c49d8ec228af56f1a69
arytontediarjo <atediarjo@gmail.com>
2021-06-24 11:40:30 -0700

    test this

--- init_py_env.sh
+++ init_py_env.sh
@@ -1,5 +1,7 @@
-python3 -m venv env\
-    && . env/bin/activate \
+python3 -m venv /root/env\
+    && . /root/env/bin/activate \
     && python3 -m pip install --upgrade pip\
+    && python3 -m pip install synapseclient\
     && python3 -m pip install git+https://github.com/arytontediarjo/PDKitRotationFeatures.git\
-    && python3 -m pip install -r requirements.txt
+    && python3 -m pip install opencv-python\
+    && python3 -m pip install imageio
