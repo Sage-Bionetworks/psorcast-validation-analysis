54d92b34d6882481961a1f2ef8b2c378c6d14542
arytontediarjo <atediarjo@gmail.com>
2021-07-01 13:01:47 -0700

    upload hand imaging finger involvement

--- feature_extraction/handImaging_finger_involvement.R
+++ feature_extraction/handImaging_finger_involvement.R
@@ -20,9 +20,29 @@ PPACMAN_TBL_ID <- "syn22337133"
 VISIT_SUMMARY <- "syn25832372"
 FINGER_LIST <- c("L1", "L2", "L3", "L4", "L5",
                  "R1", "R2", "R3", "R4", "R5")
+OUTPUT_FILENAME <- "hand_imaging_finger_status.tsv"
 
-collapse_finger_list <- function(data){
-    data$finger_nail <- purrr::map(data$finger_nail, function(x){
+############################
+# Git Reference
+############################
+SCRIPT_PATH <- file.path(
+    'feature_extraction', 
+    'handImaging_finger_involvement.R')
+GIT_TOKEN_PATH <- config::get("git")$token_path
+GIT_REPO <- config::get("git")$repo
+githubr::setGithubToken(readLines(GIT_TOKEN_PATH))
+GIT_URL <- getPermlink(
+    repository = getRepo(
+        repository = GIT_REPO, 
+        ref="branch", 
+        refName='main'), 
+    repositoryPath = SCRIPT_PATH)
+
+#' function to collapse finger list
+#' @param data dataframe containing list
+#' @return dataframe with collapsed list columns
+collapse_list <- function(data, col){
+    data$finger_nail <- purrr::map(data[[col]], function(x){
         if(length(x) == 0){
             return(NA_character_)
         }else{
@@ -30,16 +50,20 @@ collapse_finger_list <- function(data){
     return(data)
 }
 
-parse_finger_location <- function(data){
-    purrr::map(c("L1", "L2", "L3", "L4", "L5",
-                 "R1", "R2", "R3", "R4", "R5"), function(finger){
+#' Function to parse comma-separated finger 
+#' location into columns
+#' 
+#' @param data dataframe
+#' @return dataframe with finger as column
+widen_finger_string_col <- function(data, col){
+    purrr::map(FINGER_LIST, function(finger){
                      data %>%
                          dplyr::mutate(
                              !!sym(finger) := ifelse(
                                  !stringr::str_detect(
-                                     string = finger_nail,
+                                     string = !!sym(col),
                                      pattern = finger) | 
-                                     is.na(finger_nail), 0, 1)
+                                     is.na(!!sym(col)), 0, 1)
                          )}) %>% purrr::reduce(full_join) %>%
         dplyr::select(recordId, 
                       participantId, 
@@ -48,40 +72,54 @@ parse_finger_location <- function(data){
                       all_of(FINGER_LIST))
 }
 
-#' get visit reference and curated ppacman table
-visit_ref <- synGet(VISIT_REF_TBL_ID)$path %>% fread()
-ppacman <- synGet(PPACMAN_TBL_ID)$path %>% fread()
+main <- function(){
+    #' get visit reference and curated ppacman table
+    visit_ref <- synGet(VISIT_REF_TBL_ID)$path %>% fread()
+    ppacman <- synGet(PPACMAN_TBL_ID)$path %>% fread()
+    
+    #' read summary data
+    data <- synGet(VISIT_SUMMARY)$path %>% 
+        fread() %>%
+        
+        # get hand imaging from summary
+        dplyr::filter(source == "HandImaging-Curated") %>%
+        dplyr::select(recordId, participantId, createdOn) %>%
+        
+        # join with ppacman
+        join_with_ppacman(visit_ref, ppacman) %>% 
+        
+        # hard code some finger nail involvement
+        dplyr::mutate(
+            finger_nail = ifelse(
+                finger_nail == 
+                    "onycholysis L thumb", "L5", finger_nail)) %>% 
+        dplyr::mutate(
+            finger_nail = ifelse(
+                finger_nail == 
+                    "L1 (onycholysis), L2, 3 (pitting)", "L1, L2, L3", finger_nail)) %>% 
+        
+        # collapse cleaned finger nail data
+        dplyr::mutate(
+            finger_nail = stringr::str_extract_all(
+                finger_nail, (FINGER_LIST %>% paste0(collapse = "|")))) %>%
+        collapse_list("finger_nail") %>%
+        
+        # parse each finger location to columns
+        widen_finger_string_col("finger_nail") %>%
+        dplyr::mutate(createdOn = as.character(createdOn)) %>%
+        tibble::as_tibble() %>%
+        
+        # save to synapse
+        save_to_synapse(output_filename = OUTPUT_FILENAME,
+                        parent = PARENT_SYN_ID,
+                        name = "get finger status",
+                        executed = GIT_URL,
+                        used = c(VISIT_SUMMARY,
+                                 VISIT_REF_TBL_ID,
+                                 PPACMAN_TBL_ID))
+}
 
-#' read summary data
-data <- synGet(VISIT_SUMMARY)$path %>% 
-    fread() %>%
-    
-    # get hand imaging from summary
-    dplyr::filter(source == "HandImaging-Curated") %>%
-    dplyr::select(recordId, participantId, createdOn) %>%
-    
-    # join with ppacman
-    join_with_ppacman(visit_ref, ppacman) %>% 
-    
-    # hard code some finger nail involvement
-    dplyr::mutate(
-        finger_nail = ifelse(
-            finger_nail == 
-                "onycholysis L thumb", "L5", finger_nail)) %>% 
-    dplyr::mutate(
-        finger_nail = ifelse(
-            finger_nail == 
-                "L1 (onycholysis), L2, 3 (pitting)", "L1, L2, L3", finger_nail)) %>% 
-    
-    # collapse cleaned finger nail data
-    dplyr::mutate(
-        finger_nail = stringr::str_extract_all(
-            finger_nail, (FINGER_LIST %>% paste0(collapse = "|")))) %>%
-    collapse_finger_list() %>%
-    
-    # parse each finger location to columns
-    parse_finger_location() %>%
-    tibble::as_tibble()
+log_process(main(), SCRIPT_PATH)
 
 
 
